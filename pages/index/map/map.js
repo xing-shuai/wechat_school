363a9338bc2b8e24c4aa82331870b6f18cfced4c
xing-shuai <shuaihongjie@outlook.com>
2018-04-23 09:31:46 +0800

    地图导航功能完成

---
+++ pages/index/map/map.js
@@ -0,0 +1,65 @@
+
+Page({
+  data: {
+    latitude: 0,
+    longitude: 0,
+    height: 0,
+    controls: []
+  },
+  onLoad: function (options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          height: res.windowHeight,
+          controls: [{
+            id: 'mylocation',
+            iconPath: '../../../resources/icons/location.png',
+            position: {
+              left: res.windowWidth - 50,
+              top: res.windowHeight - 120,
+              width: 35,
+              height: 35
+            },
+            clickable: true
+          }]
+        })
+      },
+    })
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        that.setData({
+          latitude: latitude,
+          longitude: longitude,
+        })
+      }
+    })
+  },
+  controTap: function (e) {
+    var that = this;
+    if (e.controlId == 'mylocation') {
+      wx.getLocation({
+        type: 'wgs84',
+        success: function (res) {
+          var latitude = res.latitude;
+          var longitude = res.longitude;
+          that.setData({ latitude: latitude, longitude: longitude });
+        }
+      })
+    }
+  },
+  choose_location: function (e) {
+    wx.chooseLocation({
+      success: function (res) {
+        wx.openLocation({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          scale: 16
+        })
+      },
+    })
+  }
+})
